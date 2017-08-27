846484e4b30650e59d394e2eab99229c0f896d2d
Amine <amine@ns321058.ip-37-187-153.eu>
2017-08-27 11:45:46 +0200

    added function to test the solution of 1D ROF and corrected errors !

--- ROF.cpp
+++ ROF.cpp
@@ -1,21 +1,100 @@
 #include "ROF.h"
 
 
-ROF::ROF(const double &tau,const double &l,int lengthROF)
-// a should be a vector of positive
+ROF::ROF(const double &tau,const std::vector<double> &l)
+// this function resolve argmin( Sigma |v(i+1)-v(i)|+tau/2*Sigma|vi-li|^2) with vi on R
 {
-	 m_a=std::vector<double>(lengthROF,tau);
-	 m_b=std::vector<double>(lengthROF,tau*l);
+	m_tau=tau;
+	int lengthROF=l.size();
+	m_l=std::vector<double>(l);
+	m_a=std::vector<double>(lengthROF,tau);
+	m_b=std::vector<double>(lengthROF,tau);for (int i=0;i<l.size();i++) m_b[i]*=l[i];
 	
-	m_index_root=m_a.size();
-	m_omegaijMinus=std::deque<signed int>(m_index_root,-1);m_omegaijMinus.push_back(0);//the last element should be zero w the other elements shold be equal to 1
-	m_omegaijPlus=std::deque<signed int>(m_index_root,1);m_omegaijPlus.push_back(0);//the last element should be zero w the other elements shold be equal to 1
+	m_index_root=m_a.size()-1;
+	m_omegaijMinus=std::deque<signed int>(lengthROF-1,-1);m_omegaijMinus.push_back(0);//the last element should be zero w the other elements shold be equal to 1
+	m_omegaijPlus=std::deque<signed int>(lengthROF-1,1);m_omegaijPlus.push_back(0);//the last element should be zero w the other elements shold be equal to 1
 	computeROF();
 
 }
 
+double ROF::computeCostForArgument1DROF(const std::deque<double> &argument)
+ //argument should de declared and initialized, this function compute the value of the minimized function of the proximal operator of 1/tau*TVL for the argument argument and the cost m_g:
+ // it computes Sigma |v(i+1)-v(i)|+tau/2*Sigma|v(i)-l(i)|  with v the argument and the cost
+ {
+ 	double result=0;
+ 	// std::vector<double> l(m_b);for (int i=0;i<l.size();i++) l[i]/=m_a[i];
+	for (int i=0;i<argument.size();i++)
+	{
+		result+=0.5*m_tau*pow(argument[i]-m_l[i],2);
+	}
+	for (int i=0;i<argument.size()-1;i++)
+	{
+		result+=std::abs(argument[i+1]-argument[i]);
+	}
+	return result;
+ }
+
+ void ROF::testMinimialityOfSolution(int numberOfTests,double margin)
+{
+	double costArgmin=computeCostForArgument1DROF(m_x);
+	std::deque<double> argument;
+	bool succes=true;
+	std::cout<<"\n computing argmin of Sigma |v(i+1)-v(i)|+m_tau/2*Sigma|v(i)-m_l(i)|^2 with m_l equal to : "<<std::endl;
+		std::cout<<"  ( ";for (int i=0;i<m_l.size();i++) std::cout<<m_l[i]<<",";std::cout<<" ) \n";
+		std::cout<<" and m_tau (that should be strictly positive ) equal to "<<m_tau<<std::endl;
+
+	std::cout<<"\n the problem is equivalent to computing argmin of Sigma |v(i+1)-v(i)|+1/2*Sigma a(i)*(x(i)-b(i)/a(i))^2 \n with a equal to : "<<std::endl;
+
+	std::cout<<"  ( ";
+	for (int i=0;i<m_a.size();i++) 
+		{
+			std::cout<<m_a[i]<<",";
+		}
+	std::cout<<" ) \n";
+	for (int i=0;i<m_a.size();i++) 
+		{
+		if (m_a[i]<=0)
+			{
+				std::cout<<"all the element a(i) should be strictly positive to minimi the problem \n";return;
+			}
+		}
+	std::cout<<"with b equal to"<<std::endl;
+	std::cout<<"  ( ";for (int i=0;i<m_b.size();i++) std::cout<<m_b[i]<<",";std::cout<<" ) \n";
+
+		std::cout<<" and the solution is  equal to "<<std::endl;
+		std::cout<<"  ( ";for (int i=0;i<m_x.size();i++) std::cout<<m_x[i]<<",";std::cout<<" ) \n";
+		std::cout<<" with a cost of :"<<costArgmin<<std::endl;
+
+	srand (time(NULL));
+	for (int i=0;i<numberOfTests;i++)
+	{
+		argument=std::deque<double>(m_x);
+		for (int j=0;j<argument.size();j++) argument[j]+=(double(rand())/double(RAND_MAX)-0.5)*margin/0.5;
+
+		double costArgument=computeCostForArgument1DROF(argument);
+		
+		std::cout<<"\n cost of the minimum : "<<costArgmin<<" cost of a neighbor argument : "<<costArgument<<"      should be or not : "<<(costArgument-costArgmin>=0)<<std::endl;
+		succes=(costArgument-costArgmin>=0);
+		if(succes==false)
+			{
+				std::cout<< "problem with the last argument"<<std::endl;break;
+			}
+		std::cout<<" the argument is ( ";
+		for (int i=0;i<argument.size();i++)
+		{
+			std::cout<<argument[i]<<",";
+		}
+		std::cout<<" ) \n";
+	}
+	if (succes==true)
+	{
+		std::cout<<"\n \n for the "<<numberOfTests<<" generated arguments the minimizer succeded to minimize the cost function"<<std::endl;
+	}
+}
+
+
 ROF::ROF(const std::vector<double> &a,const std::vector<double> &b)
-// a should be a vector of positive
+// a should be a vector of strictly positive elements
 {
 	// double tau=0.5;
 	// int lArray[] = {1,1,1,1,1};
@@ -39,8 +118,10 @@ void ROF::initForwardPass()
 	m_index_current_node=0;
 	m_aBar_current_node=m_a[0];
 	m_message.push_back(0);
-	m_lambdaMinVect.push_back((m_b[0]+m_omegaijMinus[0])/(m_a[0]+m_message[0]));
-	m_lambdaPlusVect.push_back((m_b[0]+m_omegaijPlus[0])/(m_a[0]+m_message[0]));
+	m_lambdaMinusCurrentNode=(m_b[0]+m_omegaijMinus[0])/(m_a[0]+m_message[0]);
+	m_lambdaPlusCurrentNode=(m_b[0]+m_omegaijPlus[0])/(m_a[0]+m_message[0]);
+	m_lambdaMinVect.push_back(m_lambdaMinusCurrentNode);
+	m_lambdaPlusVect.push_back(m_lambdaPlusCurrentNode);
 	m_message.push_front(m_lambdaMinVect[m_lambdaMinVect.size()-1]);m_message.push_front(-m_aBar_current_node);
 	m_message.push_back(m_lambdaPlusVect[m_lambdaPlusVect.size()-1]);m_message.push_back(-m_aBar_current_node);
 }
@@ -53,7 +134,7 @@ void ROF::computeSmallestIndexAndLambdaMinus()
 	double wkiMin=m_omegaijMinus[m_index_current_node-1];
 	double wikMin=m_omegaijMinus[m_index_current_node];
 	double ai=m_a[m_index_current_node];
-	double bi=m_a[m_index_current_node];
+	double bi=m_b[m_index_current_node];
 
 	double lambdaCurrent;
 	double lambdaCurrentPlus1;
@@ -64,7 +145,7 @@ void ROF::computeSmallestIndexAndLambdaMinus()
 
 	int p=1;// thr index on m of th current lambda
 
-	while(mLambdaCurrent<wikMin and p<m_message.size())//the lowest possible index for lambda is 1 and the bigger one is  m.size()-2, if it reachs m_message.size(), it means all the elements are below wikMin
+	while(mLambdaCurrent<wikMin and p<m_message.size()-2)//the lowest possible index for lambda is 1 and the bigger one is  m.size()-2, if it reachs m_message.size(), it means all the elements are below wikMin
 	{
 		lambdaCurrent=m_message[p];
 		lambdaCurrentPlus1=m_message[p+2];
@@ -72,6 +153,14 @@ void ROF::computeSmallestIndexAndLambdaMinus()
 		mLambdaCurrent+=(sCurrent+m_aBar_current_node)*(lambdaCurrentPlus1-lambdaCurrent);
 		p+=2;// if mLambdaCurrent<wikMin the index is upper otherwise p is the value of the true index
 	}
+	// two case are possible mLambdaCurrent is still below wikMin, it means for all the breakpoints, we have mLambdaCurrent<wikMin
+	if(mLambdaCurrent<wikMin) // m_message(lambda) is below wikMin for all the breakpoints
+	{
+		p+=2;// in order to keep from the message: message <s0,lambda1,s1,....,lambdai,si,....lambdat,st> only <st> in the function updateMessage()
+	}
+	// otherwise m_message(lambda) is above for lambda equal to the last breakpoint then we keep p to its value
+
+
 	m_smallestIndexCurrentNode=p;
 	// lastComputedMLambda=mLambdaCurrent;
 	m_lambdaMinusCurrentNode=computeLambdaMinus(mLambdaCurrent);
@@ -116,7 +205,7 @@ void ROF::computeBiggestIndexAndLambdaPlus()
 	double wkiPlus=m_omegaijPlus[m_index_current_node-1];
 	double wikPlus=m_omegaijPlus[m_index_current_node];
 	double ai=m_a[m_index_current_node];
-	double bi=m_a[m_index_current_node];
+	double bi=m_b[m_index_current_node];
 
 	double lambdaCurrent;
 	double lambdaCurrentPlus1;
@@ -127,7 +216,7 @@ void ROF::computeBiggestIndexAndLambdaPlus()
 
 	int r=m_message.size()-2;// thr index on m of th current lambda
 
-	while(mLambdaCurrent>wikPlus and r>=0)//the lowest possible index for lambda is 1 and the bigger one is  m_message.size()-2, if it reachs -1, it means all the elements are above wikPlus
+	while(mLambdaCurrent>wikPlus and r>1)//the lowest possible index for lambda is 1 and the bigger one is  m_message.size()-2, if it reachs -1, it means all the elements are above wikPlus
 	{
 		r-=2;// if mLambdaCurrent>wikPlus the index is lower otherwise r is the value of the true index
 		lambdaCurrent=m_message[r];
@@ -135,6 +224,14 @@ void ROF::computeBiggestIndexAndLambdaPlus()
 		sCurrent=m_message[r+1];
 		mLambdaCurrent-=(sCurrent+m_aBar_current_node)*(lambdaCurrentPlus1-lambdaCurrent);
 	}
+	// two case are possible mLambdaCurrent is still above wikPlus, it means for all the breakpoints, we have mLambdaCurrent>wikPlus
+	if(mLambdaCurrent>wikPlus) // m_message(lambda) is above wikPlus for all the breakpoints
+	{
+		r-=2;// in order to keep from the message: message <s0,lambda1,s1,....,lambdai,si,....lambdat,st> only <s0> in the function updateMessage()
+	}
+	// otherwise m_message(lambda) is under for lambda equal to the smallest breakpoint then we keep r to its value
+
+
 		m_biggestIndexCurrentNode=r;
 		// double lastComputedMLambda=mLambdaCurrent;
 		m_lambdaPlusCurrentNode=computeLambdaPlus(mLambdaCurrent);
@@ -152,9 +249,9 @@ double ROF::computeLambdaPlus(const double &lastComputedMLambda)
 		double mLambdaMin=lastComputedMLambda;// the value of the message for the smallest value of lambda
 		double lambdaMin=m_message[m_biggestIndexCurrentNode+2];
 
-		double slopeBeforeLambdaMax=m_message[m_smallestIndexCurrentNode+1]+m_aBar_current_node;
+		double slopeBeforeLambdaMin=m_message[m_biggestIndexCurrentNode+1]+m_aBar_current_node;
 
-		lambdaPlusCurrentNode=lambdaMin+(wijPlus-mLambdaMin)/(slopeBeforeLambdaMax);// lambdaMax is after the biggest breakpoint
+		lambdaPlusCurrentNode=lambdaMin+(wijPlus-mLambdaMin)/(slopeBeforeLambdaMin);// lambdaMax is after the biggest breakpoint
 	}	
 	else // m_smallestIndexCurrentNode is between 1 and m.size()-2
 		// the last computed m_message(lambda) is above wijMinus so lambdaMax is juste below the breakpoint
@@ -176,11 +273,11 @@ void ROF::computeROF()
 	
 	initForwardPass();
 
-	printInformationsForNode();
+	// printInformationsForNode();
 	for (m_index_current_node=1;m_index_current_node<=m_index_root;m_index_current_node++)
 	{
 		iterateForwardPass();
-		printInformationsForNode();
+		// printInformationsForNode();
 
 	}
 	computeROFBackwardPass();
@@ -202,7 +299,7 @@ void ROF::iterateForwardPass()
 void ROF::computeROFBackwardPass()
 //const std::vector<double> &lambdaMinVect,const std::vector<double> &lambdaPlusVect,std::deque<double> &x)
 {
-	if ( (m_lambdaMinVect[m_lambdaMinVect.size()-1]-m_lambdaPlusVect[m_lambdaPlusVect.size()-1])>0.0001 )
+	if ( std::abs(m_lambdaMinVect[m_lambdaMinVect.size()-1]-m_lambdaPlusVect[m_lambdaPlusVect.size()-1])>0.0001 )
 			{
 
 		throw std::invalid_argument( "problem with the forward Pass: lambdaMin(n,n+1) and lambdaMax(n,n+1) should be equal" );
@@ -255,7 +352,9 @@ void ROF::updateMessage()
 
 std::deque<double> ROF::getSolution(bool printIt)
 {
-	std::cout<<"\n minimization of f with f(x)=Sigma_{i}(fi(xi))+Sigma_{i,i+1}(f_{i,i+1}(x_{i+1}-x{i})) \n and fi of the form 1/2 (a_{i}x^2-b{i}x and f_{i,i+1}(x) of the form |x|)";
+	if(printIt==true)
+	{
+	std::cout<<"\n minimization of f with f(x)=Sigma_{i}(fi(xi))+Sigma_{i,i+1}(f_{i,i+1}(x_{i+1}-x{i})) \n and fi of the form 1/2*a_{i}x^2-b{i}x and f_{i,i+1}(x) of the form |x|)";
 	
 	std::cout<<"\n here we have the following values for a_{i}";
 	std::cout<<"( ";
@@ -274,8 +373,7 @@ std::deque<double> ROF::getSolution(bool printIt)
 	std::cout<<" ) \n";
 
 	std::cout<<"\n the solution of the onedimensionnal ROF problem "<<"\n";
-	if(printIt==true)
-	{
+	
 		std::cout<<"( ";
 		for (int i=0;i<m_x.size();i++)
 		{
--- ROF.h
+++ ROF.h
@@ -5,12 +5,13 @@
 #include <stdexcept>
 #include <stdlib.h>
 #include <iostream>
+#include "MatchingAlgorithm.h"
 
 
 class ROF 
 {
 	public:
-		ROF(const double &tau,const double &l,int lengthROF);
+		ROF(const double &tau,const std::vector<double> &l);
 		ROF(const std::vector<double> &a,const std::vector<double> &b);
 		void computeROF();
 		void initForwardPass();
@@ -26,10 +27,13 @@ class ROF
 		void printInformationsForNode();
         static double clip(const double &x,const double &lambdaMinus,const double &lambdaPlus);
         std::deque<double> getSolution(bool printIt=false);
+        double computeCostForArgument1DROF(const std::deque<double> &argument);
+        void testMinimialityOfSolution(int numberOfTests,double margin);
 
 	private:
 		std::vector<double> m_a;
 		std::vector<double> m_b;
+		std::vector<double> m_l;
 		std::deque<double> m_x;// the result
 		std::deque<double> m_message;
 		// the vector m_message is a reprentation for the message mi in the form <s0,lambda1,s1,....,lambdai,si,....lambdat,st>, m_message represents a  piecewise affine function
@@ -47,7 +51,7 @@ class ROF
 		signed int m_smallestIndexCurrentNode;signed int m_biggestIndexCurrentNode;// respectively the index of the smallest breakpoints above wijMin and the biggest breakpoints under wijPlus, with wijMin=omegaijMinus[m_index_current_node] wijPlus=omegaijPlus[m_index_current_node], at the nd of the iteration
 
 		double m_lambdaMinusCurrentNode;double m_lambdaPlusCurrentNode; // defined by message(m_lambdaMin_current_node)=wijMin and message(m_lambdaPlus_current_node)=wijPlus
-
+		double m_tau;
 		// double m_lastComputedMLambda;// the last computed value of m(lambda) during the iterations
 };
 
